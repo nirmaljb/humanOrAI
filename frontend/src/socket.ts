d7b029423a3b72991de006cbeedb696049a3b391
Nirmal Jyoti Biswas <kaizenjb@Nirmals-MacBook-Air.local>
2025-04-27 22:48:30 +0530

    v1

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:8000';
+const URL = 'humanorai-production.up.railway.app';
 
 const socket = io(URL, {
     autoConnect: false,
