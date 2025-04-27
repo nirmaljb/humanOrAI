acaed64619bce37ecd4b963eaf7dd28bd998d3ad
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-04-27 23:39:22 +0530

    v2

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,8 +12,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        // origin: 'https://human-or-ai-roan.vercel.app'
-        origin: 'http://localhost:5173'
+        origin: 'https://human-or-ai-roan.vercel.app'
+        // origin: 'http://localhost:5173'
     }
 });
 
