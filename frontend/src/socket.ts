9cd8a63205f41c4bc7bf83d17fb6edd1a3984327
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-06-14 16:15:17 +0530

    testing ci/cd

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://humanorai-production.up.railway.app';
+// const URL = 'https://humanorai-production.up.railway.app';
 // const URL = "http://localhost:8000"
+const URL = 'http://ec2-13-235-68-87.ap-south-1.compute.amazonaws.com/'
 
 const socket = io(URL, {
     autoConnect: false,
